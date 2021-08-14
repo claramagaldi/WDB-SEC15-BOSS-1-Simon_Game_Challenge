fe545212c1e39549b703d9540a0fbda842ee272a
Clara Magaldi Rodrigues <claramagaldi@gmail.com>
2021-08-14 18:12:56 -0400

    Step 2 - Corrections

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = []
 
-function nextSequence () {
+console.log(buttonColours);
+console.log(gamePattern);
+
+function nextSequence() {
   var randomNumber = Math.floor(Math.random()*4);
+  return randomNumber;
 }
 
-var randomChosenColour = buttonColours[randomNumber];
+var randomChosenColour = buttonColours[nextSequence()];
 
 gamePattern.push(randomChosenColour);
+
+console.log(gamePattern);
