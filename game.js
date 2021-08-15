2dd6ea3dede13618944ee540d19e708015269095
Clara Magaldi Rodrigues <claramagaldi@gmail.com>
2021-08-15 18:59:01 -0400

    Step 10 - Restart the game

--- game.js
+++ game.js
@@ -1,14 +1,73 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
-var gamePattern = []
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+$(document).keydown(function() {
+  if (gamePattern.length === 0) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+  }
+});
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  console.log(userClickedPattern);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) { //checks if the user finished his sequence
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over")
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
 
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random()*4);
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  //playSound(randomChosenColour);
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   console.log(audio);
+  //audio.play();
+  animatePress(randomChosenColour);
+};
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-nextSequence();
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+}
